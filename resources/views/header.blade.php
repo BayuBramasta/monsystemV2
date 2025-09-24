916e2311695b520f66e7b1bc02d941ed23c4882a
Rovalian <chelseaoktaviano01@gmail.com>
2025-09-24 09:06:04 +0700

    Frontend

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -34,7 +34,6 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto py-4 py-lg-0">
                 <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="/">Home</a></li>
-                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="lab">Lab</a></li>
                 <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="teknisi">Teknisi</a></li>
                 <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="status">Status</a></li>
                 <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="reservasi">Rerservation</a></li>
--- resources/views/status.blade.php
+++ resources/views/status.blade.php
@@ -5,30 +5,7 @@
 <main class="container-fluid mt-5 mb-4">
     <div class="container-fluid px-4 px-lg-5">
         <div class="row gx-4 gx-lg-5 justify-content-center">
-            <div class="col-md-10 mt-5">
-                <table id="example" class="table table-striped nowrap">
-                    <thead>
-                        <tr>
-                            <th>No.</th>
-                            <th>Nama</th>
-                            <th>NRP/NIP</th>
-                            <th>Status</th>
-                            <th>Detail</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($peminjaman as $data)
-                        <tr>
-                            <td>{{ $loop -> iteration }}</td>
-                            <td>{{ $data -> nama_mahasiswa }}</td>
-                            <td>{{ $data -> nrp }}</td>
-                            <td>{{ $data -> status }}</td>
-                            <td><a href="{{ route('detail.show',['id'=>$data->id_peminjaman]) }}" class="btn btn-sm btn-primary">Detail</a></td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+           <div class></div>
         </div>
     </div>
 </main>
