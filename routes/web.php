08978250c70086a5bb80a4ee0bebc0173353181a
BayuBramasta <bayubramasta125@gmail.com>
2025-09-24 15:47:50 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('login');
 });
+Route::get('/lab', function () {
+    return view('lab');
+});
 Route::get('/reservasi', [peminjamanController::class, 'create'])->name('reservasi.create');
 Route::post('/reservasi', [peminjamanController::class, 'store'])->name('reservasi.store');
 Route::get('/status', [peminjamanController::class, 'index'])->name('status.index');
